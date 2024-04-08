8d18bf4316f7b4dcaa473568e238e781131bff3c
barisscl <barisceylan907@gmail.com>
2024-04-08 14:38:27 +0300

    Custom Component set in the code.

--- App.js
+++ App.js
@@ -1,36 +1,13 @@
 import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { View, StyleSheet } from "react-native";
+import Card from './src/components/Card';
 
 function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.card_container}>
-        <View style={styles.card_body}>
-          <Text style={styles.card_title}>Eddard Start</Text>
-          <Text style={styles.card_text}>Winter is Coming !!</Text>
-        </View>
-        <TouchableOpacity style={styles.card_button_container}>
-          <Text style={styles.card_button_title}>LIKED</Text>
-        </TouchableOpacity>
-      </View>
-      <View style={styles.card_container}>
-        <View style={styles.card_body}>
-          <Text style={styles.card_title}>Arif Işık</Text>
-          <Text style={styles.card_text}>Uzaylılar tarafından kaçırıldım, evet tarafından</Text>
-        </View>
-        <TouchableOpacity style={styles.card_button_container}>
-          <Text style={styles.card_button_title}>LIKED</Text>
-        </TouchableOpacity>
-      </View>
-      <View style={styles.card_container}>
-        <View style={styles.card_body}>
-          <Text style={styles.card_title}>Serbest</Text>
-          <Text style={styles.card_text}>İlhami abi sen söyle, ben başka bir ilde miyim?</Text>
-        </View>
-        <TouchableOpacity style={styles.card_button_container}>
-          <Text style={styles.card_button_title}>LIKED</Text>
-        </TouchableOpacity>
-      </View>
+      <Card title="Eddard Stark" text="Winter Is Coming !!"/>
+      <Card title="Arif Işık" text="Uzaylılar tarafından kaçırıldım, evet tarafından"/>
+      <Card title="Serbest" text="İlhami abi sen söyle, ben başka bir ilde miyim?"/>
     </View>
   );
 }
@@ -39,38 +16,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#e0e0e0',
-  },
-  card_container: {
-    backgroundColor: 'white',
-    margin: 30,
-    padding: 10,
-    borderRadius: 10,
-    borderColor: 'black',
-  },
-  card_body: {
-    padding: 10,
-  },
-  card_title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    margin: 10,
-    marginBottom: 3,
-  },
-  card_text: {
-    fontSize: 15,
-    margin: 10,
-    marginTop: 3,
-    marginLeft: 30,
-  },
-  card_button_container: {
-    backgroundColor: '#00C2FF',
-    padding: 10,
-    alignItems: 'center',
-    borderRadius: 10,
-  },
-  card_button_title: {
-    fontWeight: 'bold',
-    color: 'white',
   },
 })
 export default App;
---
+++ src/components/Card/Card.style.js
@@ -0,0 +1,38 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    margin: 30,
+    padding: 10,
+    borderRadius: 10,
+    borderColor: "black",
+  },
+  body: {
+    padding: 10,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    margin: 10,
+    marginBottom: 3,
+  },
+  text: {
+    fontSize: 15,
+    margin: 10,
+    marginTop: 3,
+    marginLeft: 30,
+  },
+  button_container: {
+    backgroundColor: "#00C2FF",
+    padding: 10,
+    alignItems: "center",
+    borderRadius: 10,
+  },
+  button_title: {
+    fontWeight: "bold",
+    color: "white",
+  },
+});
+
+export default styles;
